b6e8a5388e16479d0329a2c82103afa00417d402
Aagam Bakliwal <aagam.b@gmail.com>
2025-02-24 20:06:54 -0500

    Initial Commit

---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by Aagam Bakliwal on 2/24/25.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
